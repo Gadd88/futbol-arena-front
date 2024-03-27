6246e337ed29ffbcdb42bf7166b069656cc95570
JoacooReyes <joacooreyes@gmail.com>
2024-03-27 02:30:56 -0300

    comienzo a trabajar sobre branch reservas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./components"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Contacto, Error404, Galeria, Home, Nosotros, Registro } from "./views"
+import { Contacto, Error404, Galeria, Home, Nosotros, Registro, Reservas } from "./views"
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
         <Route path='/galeria' element={<Galeria/>}/>
         <Route path='/contacto' element={<Contacto/>}/>
         <Route path='/registro' element={<Registro/>}/>
+        <Route path='/reservas' element={<Reservas/>}/>
         <Route path='/*' element={<Error404/>}/>
       </Routes>
     </BrowserRouter>
