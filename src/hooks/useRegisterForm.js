942c96d8c80e5559ed0c67e4bb5acd35d1235941
Pablo Moyano <pablomoyano08@gmail.com>
2024-03-30 13:30:19 -0300

    actualizacion registro

---
+++ src/hooks/useRegisterForm.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const useRegisterForm = (callback) => {
+  const [inputs, setInputs] = useState({
+    fullName: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleSubmit = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    if (inputs.password === inputs.confirmPassword) {
+      callback();
+    } else {
+      alert('Passwords do not match');
+    }
+  };
+
+  const handleInputChange = (event) => {
+    event.persist();
+    setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+  };
+
+  return {
+    handleSubmit,
+    handleInputChange,
+    inputs,
+  };
+};
+
+export default useRegisterForm;
--- src/views/registro/Registro.jsx
+++ src/views/registro/Registro.jsx
@@ -1,8 +1,62 @@
-
+import React from 'react';
+import useRegisterForm from '../../hooks/useRegisterForm';
 export const Registro = () => {
+  const register = () => {
+    //lógica para enviar los datos al backend
+    console.log('User registered:', inputs);
+  };
+
+  const { inputs, handleInputChange, handleSubmit } = useRegisterForm(register);
+
   return (
     <div className="w-full mx-auto h-[100dvh] lg:w-[1200px] bg-arena-green-50 rounded-xl overflow-hidden">
-      
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4">
+        <input
+          type="text"
+          name="fullName"
+          onChange={handleInputChange}
+          value={inputs.fullName}
+          placeholder="Nombre completo"
+          className="p-2 border border-gray-300 text-gray-700"
+        />
+        <input
+          type="email"
+          name="email"
+          onChange={handleInputChange}
+          value={inputs.email}
+          placeholder="Email"
+          className="p-2 border border-gray-300 text-gray-700"
+        />
+        <input
+          type="tel"
+          name="phone"
+          onChange={handleInputChange}
+          value={inputs.phone}
+          placeholder="Teléfono"
+          className="p-2 border border-gray-300 text-gray-700"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handleInputChange}
+          value={inputs.password}
+          placeholder="Password"
+          className="p-2 border border-gray-300 text-gray-700"
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          onChange={handleInputChange}
+          value={inputs.confirmPassword}
+          placeholder="Re ingreso Password"
+          className="p-2 border border-gray-300 text-gray-700"
+        />
+        <button type="submit" className="bg-blue-500 text-white p-2">
+          Registro
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default Registro;
