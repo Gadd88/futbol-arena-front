df1c2ca8e972d6679c51740bffc3bbed9b282a90
Enzo Castro <enzocastro110@gmail.com>
2024-04-06 23:50:36 -0300

    lógica arreglada, sigue haciendo scroll y las imágenes se pasan del contenedor

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,17 +5,22 @@ export const Banner = () => {
     const listRef = useRef();
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const scrollToImage = (direction) => {
-        if (direction === 'prev') {
-            setCurrentIndex(curr => {
-                const isFirstSlide = currentIndex === 0;
-                return isFirstSlide ? 0 : curr - 1;
+    useEffect(() => {
+        const listNode = listRef.current;
+        const imgNode = listNode.querySelectorAll("figure")[currentIndex];
+        if (imgNode) {
+            imgNode.scrollIntoView({
+                behavior: "smooth"
             });
+        }
+    }, [currentIndex]);
+
+    const scrollToImage = (direction, event) => {
+        event.preventDefault();
+        if (direction === 'prev') {
+            setCurrentIndex(curr => curr === 0 ? data.length - 1 : curr - 1);
         } else {
-            const isLastSlide = currentIndex === data.length - 1;
-            if (!isLastSlide) {
-                setCurrentIndex(curr => curr + 1);
-            }
+            setCurrentIndex(curr => curr === data.length - 1 ? 0 : curr + 1);
         }
     };
 
@@ -25,30 +30,26 @@ export const Banner = () => {
 
     return (
         <>
-            <div className="w-full h-[400px] overflow-hidden rounded-md">
-                <div className="h-full relative">
+            <div className="w-full h-[400px] overflow-hidden rounded-md relative">
+                <div className="absolute top-1/2 left-8 text-4xl text-white cursor-pointer" onClick={(event) => scrollToImage('prev', event)}>&#8656;</div>
 
-                    <div className="absolute top-1/2 translate-x-0 translate-y-[50%] left-8 text-4xl text-white cursor-pointer" onClick={() => scrollToImage('prev')}>&#8656;</div>
+                <div className="absolute top-1/2 right-8 text-4xl text-white cursor-pointer" onClick={(event) => scrollToImage('next', event)} >&#8658;</div>
 
-                    <div className="absolute top-1/2 translate-x-0 translate-y-[50%] right-8 text-4xl text-white cursor-pointer" onClick={() => scrollToImage('next')} >&#8658;</div>
-
-                    <div className="w-full h-full overflow-hidden" ref={listRef}>
-                            {
-                                data.map((item) => (
-                                        <figure key={item.id}>
-                                            <img className="w-full h-full object-cover" src={item.imgUrl}alt={`banner-${item.id}`} />
-                                        </figure>
-                                ))
-                            }
-                    </div>
-                    <div className="flex justify-center items-center absolute bottom-10 left-0 right-0 mx-auto">
-                        {
-                            data.map((_, idx)=>(
-                                <div key={idx} className={`${idx === currentIndex ? 'bg-arena-green-400':"bg-slate-300"} my-2 mx-1 cursor-pointer text-center  w-4 h-4 rounded-full`} onClick={() => goToSlide(idx)}>  
-                                    </div>
-                            ))
-                        }
-                    </div>
+                <div className="w-full h-full overflow-hidden" ref={listRef}>
+                    {
+                        data.map((item, idx) => (
+                            <figure key={item.id} className={`${idx === currentIndex ? 'block' : 'hidden'}`}>
+                                <img className="w-full h-full object-cover" src={item.imgUrl} alt={`banner-${item.id}`} />
+                            </figure>
+                        ))
+                    }
+                </div>
+                <div className="flex justify-center items-center absolute bottom-10 left-0 right-0 mx-auto">
+                    {
+                        data.map((_, idx) => (
+                            <div key={idx} className={`${idx === currentIndex ? 'bg-arena-green-400' : 'bg-slate-300'} my-2 mx-1 cursor-pointer text-center w-4 h-4 rounded-full`} onClick={() => goToSlide(idx)}></div>
+                        ))
+                    }
                 </div>
             </div>
         </>
