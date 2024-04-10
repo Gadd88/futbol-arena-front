5edfcc8abcc0608c65bf9033872e1569ef260d84
Enzo Castro <enzocastro110@gmail.com>
2024-04-10 17:01:41 -0300

    creación de cart, aún falta lógica con mapeo de cards

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,8 +1,9 @@
+import { MisReservas } from "../../components/misReservas/MisReservas"
 
 export const Home = () => {
   return (
     <div className="w-full mx-auto h-[100dvh] xl:w-[1200px] bg-arena-green-50 rounded-xl overflow-hidden">
-      
+      <MisReservas/>
     </div>
   )
 }
