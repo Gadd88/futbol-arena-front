296afc64d030c1c3b1f4d187a167a86f48423da7
Enzo Castro <enzocastro110@gmail.com>
2024-04-10 13:31:29 -0300

    creación de componente

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export * from './header/Header.jsx'
+export * from './misReservas/MisReservas.jsx'
---
+++ src/components/misReservas/MisReservas.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MisReservas = () => {
+  return (
+    <>
+    <div>Seré el card</div>
+    </>
+  )
+}
