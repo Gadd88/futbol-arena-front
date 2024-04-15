3863619016f80fead8b30a3e0219c900c14f9f07
Gabriela <maria.g.monteros@gmail.com>
2024-04-15 16:15:07 -0300

    estilos de cards

--- src/components/servicios/Servicios.jsx
+++ src/components/servicios/Servicios.jsx
@@ -10,7 +10,7 @@ export const Servicios = () => {
 
     <>   
 
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-2 p-2 bg-black text-sm overflow-hidden mx-auto max-w-screen-xl mb-8 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-2 p-2 bg-black text-sm  mx-auto max-w-screen-xl">
       <ServiciosCards title="LA TIENDA" description="Encontra lo que estas buscando!" image={fotoCarrito}/>
       <ServiciosCards title="TORNEOS Y COMPETENCIAS" description="Participa en emocionantes torneos y competencias en nuestras canchas de fútbol. Demostra tu talento y competi por grandes premios. ¡No te lo pierdas!" image={fotoPelota} />
       <ServiciosCards title="RESERVA AHORA!" description="¿Cuando y Donde? Ingresa el tipo de cancha, la fecha, la hora y descubri todas las opciones que tenemos para vos." image={fotoReserva} />
--- src/components/serviciosCards/ServiciosCards.jsx
+++ src/components/serviciosCards/ServiciosCards.jsx
@@ -5,7 +5,7 @@ import { Card } from "flowbite-react";
 export const ServiciosCards = ({title, description, image}) => { return (
 
   <>
-  <Card className="max-w-sm" imgAlt="foto"> 
+  <Card className="max-w-sm mb-4" imgAlt="foto"> 
   <img src={image} alt="" className="object-cover w-full h-full mx-auto"/>
   <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
   <p className="font-normal text-gray-700 dark:text-gray-400" >{description}</p>
