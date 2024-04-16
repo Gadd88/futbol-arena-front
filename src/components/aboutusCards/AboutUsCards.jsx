e4ff174f06f74d8c2177a3bfe5608bc05e059fe2
JoacooReyes <joacooreyes@gmail.com>
2024-04-16 03:42:33 -0300

    agrego cambios esteticos a las cards

--- src/components/aboutusCards/AboutUsCards.jsx
+++ src/components/aboutusCards/AboutUsCards.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const AboutUsCards = () => {
   return (
-    <section className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4 m-3 mt-10 ">
-      <div className="container border border-2 rounded-lg content-center box-border h-200 w-100 p-4 justify-center">
-        <div>
+    <section className="grid lg:grid-cols-5 md:grid-cols-2 sm:grid-cols-1 gap-4 m-10 ">
+      <div className="flex flex-col border border-1 p-4 rounded-lg h-200 w-100 p-4 justify-items-center justify-center">
+        <div className="flex justify-center justify-items-center">
           <img
             src="https://picsum.photos/200/200"
             alt=""
@@ -14,13 +14,13 @@ const AboutUsCards = () => {
         <h3 className="text-arena-green-700 font-bold text-center text-xl mb-4">
           Juan Perez
         </h3>
-        <p className="text-arena-green-700 font-bold text-center text-l">
+        <p className="text-gray-400 italic text-center text-l">
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
           reiciendis fuga labore aliquid magnam! Ipsa?"
         </p>
       </div>
-      <div className="container border border-2">
-        <div>
+      <div className="flex flex-col border border-1 rounded-lg p-4 h-200 w-100 p-4 justify-items-center justify-center">
+        <div className="flex justify-center justify-items-center">
           <img
             src="https://picsum.photos/200/200"
             alt=""
@@ -30,13 +30,13 @@ const AboutUsCards = () => {
         <h3 className="text-arena-green-700 font-bold text-center text-xl mb-4">
           Juan Perez
         </h3>
-        <p className="text-arena-green-700 font-bold text-center text-l">
+        <p className="text-gray-400 italic text-center text-l">
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
           reiciendis fuga labore aliquid magnam! Ipsa?"
         </p>
       </div>
-      <div className="container border border-2">
-        <div>
+      <div className="flex flex-col border border-1 rounded-lg p-4 h-200 w-100 p-4 justify-items-center justify-center">
+        <div className="flex justify-center justify-items-center">
           <img
             src="https://picsum.photos/200/200"
             alt=""
@@ -46,13 +46,13 @@ const AboutUsCards = () => {
         <h3 className="text-arena-green-700 font-bold text-center text-xl mb-4">
           Juan Perez
         </h3>
-        <p className="text-arena-green-700 font-bold text-center text-l">
+        <p className="text-gray-400 italic text-center text-l">
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
           reiciendis fuga labore aliquid magnam! Ipsa?"
         </p>
       </div>
-      <div className="container border border-2">
-        <div>
+      <div className="flex flex-col border border-1 rounded-lg p-4 h-200 w-100 p-4 justify-items-center justify-center">
+        <div className="flex justify-center justify-items-center">
           <img
             src="https://picsum.photos/200/200"
             alt=""
@@ -62,7 +62,23 @@ const AboutUsCards = () => {
         <h3 className="text-arena-green-700 font-bold text-center text-xl mb-4">
           Juan Perez
         </h3>
-        <p className="text-arena-green-700 font-bold text-center text-l">
+        <p className="text-gray-400 italic text-center text-l">
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          reiciendis fuga labore aliquid magnam! Ipsa?"
+        </p>
+      </div>
+      <div className="flex flex-col border border-1 rounded-lg p-4 rounded-lg h-200 w-100 p-4 justify-items-center justify-center">
+        <div className="flex justify-center justify-items-center">
+          <img
+            src="https://picsum.photos/200/200"
+            alt=""
+            className="rounded-full mb-3"
+          />
+        </div>
+        <h3 className="text-arena-green-700 font-bold text-center text-xl mb-4">
+          Juan Perez
+        </h3>
+        <p className="text-gray-400 italic text-center text-l">
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
           reiciendis fuga labore aliquid magnam! Ipsa?"
         </p>
--- src/views/nosotros/Nosotros.jsx
+++ src/views/nosotros/Nosotros.jsx
@@ -3,7 +3,7 @@ import AboutUsCards from "../../components/aboutusCards/AboutUsCards";
 
 export const Nosotros = () => {
   return (
-    <div className="w-full mx-auto h-[100dvh] xl:w-[1200px] bg-arena-green-50 rounded-xl overflow-hidden">
+    <div className="w-full mx-auto  xl:w-[1200px] bg-arena-green-50 rounded-xl overflow-hidden">
       <AboutUsBanner />
       <AboutUsCards />
     </div>
