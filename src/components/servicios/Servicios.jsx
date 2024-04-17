cd36c2bcc25264d1e9b4b3c01740d089f2c9c614
Gabriela <maria.g.monteros@gmail.com>
2024-04-17 16:47:54 -0300

    solo falta arreglar tamaño de una card

--- src/components/servicios/Servicios.jsx
+++ src/components/servicios/Servicios.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ServiciosCards } from '../serviciosCards/ServiciosCards.jsx'
 import fotoPelota  from '../../assets/fotoPelota.png'
-import fotoCarrito from '../../assets/carrito.png'
+import fotoTienda from '../../assets/fotoTienda.png'
 import fotoReserva from '../../assets/fotoReserva.png'
 import fotoBar from '../../assets/fotoBar.png'
 
@@ -10,14 +10,15 @@ export const Servicios = () => {
 
     <>   
 
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-2 p-2 bg-black text-sm  mx-auto max-w-screen-xl">
-      <ServiciosCards title="LA TIENDA" description="Encontra lo que estas buscando!" image={fotoCarrito}/>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2  gap-2 p-2 justify-center text-sm h-full w-full">
+      <ServiciosCards title="LA TIENDA" description="Encontra lo que estas buscando!" image={fotoReserva}/>
       <ServiciosCards title="TORNEOS Y COMPETENCIAS" description="Participa en emocionantes torneos y competencias en nuestras canchas de fútbol. Demostra tu talento y competi por grandes premios. ¡No te lo pierdas!" image={fotoPelota} />
-      <ServiciosCards title="RESERVA AHORA!" description="¿Cuando y Donde? Ingresa el tipo de cancha, la fecha, la hora y descubri todas las opciones que tenemos para vos." image={fotoReserva} />
-      <ServiciosCards title="BAR" description="Visita nuestro bar y disfruta de un espacio animado para relajarte y disfrutar de nuestras ofertas gastronomicas. ¡El lugar perfecto para compartir momentos inolvidables con amigos!" image={fotoBar} />
+      <ServiciosCards title="RESERVA AHORA!" description="Ingresa el tipo de cancha, la fecha, la hora y descubri todas las opciones que tenemos para vos." image={fotoTienda} />
+      <ServiciosCards title="BAR" description="Visita nuestro bar y disfruta de nuestras ofertas gastronomicas. ¡El lugar perfecto para compartir momentos inolvidables con amigos!" image={fotoBar} />
     
     </div>
-    
+   
+
     </>
   )
 }
--- src/components/serviciosCards/ServiciosCards.jsx
+++ src/components/serviciosCards/ServiciosCards.jsx
@@ -5,10 +5,10 @@ import { Card } from "flowbite-react";
 export const ServiciosCards = ({title, description, image}) => { return (
 
   <>
-  <Card className="max-w-sm mb-4" imgAlt="foto"> 
-  <img src={image} alt="" className="object-cover w-full h-full mx-auto"/>
-  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-  <p className="font-normal text-gray-700 dark:text-gray-400" >{description}</p>
+  <Card className="max-w-sm h-[300px]  mx-auto relative hover:border-green-950 overflow-hidden" imgAlt="foto"> 
+  <img src={image} alt="foto" className="absolute top-0 left-0 z-0 object-cover w-full h-full mx-auto rounded"/>
+  <h5 className="text-2xl font-bold tracking-tight text-white dark:text-white z-10">{title}</h5>
+  <p className="font-normal text-white dark:text-gray-400 z-10 hover:text-black hover:font-bold hover:text-lg" >{description}</p>
   </Card>
   
   </>
