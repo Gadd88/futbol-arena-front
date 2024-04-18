474b5fbcee254906a4c27ec9dcbe1e611d65b038
Matias Saade <matiassaade@gmail.com>
2024-04-18 15:53:25 -0300

    fix: arreglado issue al eliminar reservas

--- src/views/reservas/Reservas.jsx
+++ src/views/reservas/Reservas.jsx
@@ -4,7 +4,7 @@ import { useCancha } from '../../hooks'
 export const Reservas = () => {
 
   const {handleDate, handleConsulta, handleTime, reservation} = useCancha()
-  
+  console.log(reservation)
   return (
     <section className='flex flex-col items-center justify-center h-full relative'>
       <article className="w-full mx-auto h-full bg-arena-green-50 rounded-xl p-5 flex flex-wrap gap-5 lg:p-10 lg:grid lg:grid-cols-3 lg:place-content-center justify-center items-stretch">
@@ -14,7 +14,7 @@ export const Reservas = () => {
       </article>
       {
         reservation.reservation_time != '' &&
-        <ReservationResume/>
+        <ReservationResume reservation={reservation}/>
       }
     </section>
   )
