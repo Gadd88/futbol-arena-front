735f1c49601f970d847a7ac764acc0ef7467f8f9
Matias Saade <matiassaade@gmail.com>
2024-04-18 16:18:33 -0300

    feat: mergeado con seccion de about us

---
+++ src/components/aboutusCards/aboutUsTexts.js
@@ -0,0 +1,22 @@
+export const groupInfo = [
+    {
+        nombre: 'Matias Saade',
+        texto: "Gracias Javascript 🤣."
+    },
+    {
+        nombre: 'Joaquin Reyes',
+        texto: "Despues de un largo dia de trabajo con el codigo, no hay nada como tomarse un rico cafe."
+    },
+    {
+        nombre: 'Gabriela Monteros',
+        texto: "Tuve que elegir entre el futbol o la programacion, asi que hice una página web de futbol.."
+    },
+    {
+        nombre: 'Pablo Moyano',
+        texto: "No me pude decidir por uno de los dos, asi que me hice FullStack :)"
+    },
+    {
+        nombre: 'Enzo Castro',
+        texto: "La programacion es mi pasion, me encanta el front-end!"
+    },
+]
--- src/views/nosotros/Nosotros.jsx
+++ src/views/nosotros/Nosotros.jsx
@@ -1,11 +1,18 @@
-import AboutUsBanner from "../../components/aboutusBanner/AboutUsBanner";
-import AboutUsCards from "../../components/aboutusCards/AboutUsCards";
+import {AboutUsBanner, AboutUsCards} from "../../components/";
+import { groupInfo } from "../../components/aboutusCards/aboutUsTexts";
 
 export const Nosotros = () => {
   return (
-    <div className="w-full mx-auto  xl:w-[1200px] bg-arena-green-50 rounded-xl overflow-hidden">
+    <main className="w-full mx-auto  xl:w-[1200px] bg-arena-green-50 rounded-xl overflow-hidden">
       <AboutUsBanner />
-      <AboutUsCards />
-    </div>
+      <section className="grid lg:grid-cols-5 md:grid-cols-2 sm:grid-cols-1 gap-4 m-10 ">
+        {
+          groupInfo.map((integrante, idx) => (
+            <AboutUsCards key={idx} nombre={integrante.nombre} texto={integrante.texto}/>
+
+          ))
+        }
+      </section>
+    </main>
   );
 };
