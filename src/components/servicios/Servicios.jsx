cdcb5c71fa8081f51e78d2f86467a29aa812f110
Gabriela <maria.g.monteros@gmail.com>
2024-04-19 09:08:15 -0300

    servicios con estilos

--- src/components/servicios/Servicios.jsx
+++ src/components/servicios/Servicios.jsx
@@ -10,7 +10,7 @@ export const Servicios = () => {
 
     <>   
 
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2  gap-2 p-2 justify-center text-sm h-full w-full">
+    <div className="grid grid-cols-1 grid-rows-2 sm:grid-cols-2 lg:grid-cols-2 gap-x-4 gap-y-4 p-2 text-sm ">
       <ServiciosCards title="LA TIENDA" description="Encontra lo que estas buscando!" image={fotoReserva}/>
       <ServiciosCards title="TORNEOS Y COMPETENCIAS" description="Participa en emocionantes torneos y competencias en nuestras canchas de fútbol. Demostra tu talento y competi por grandes premios. ¡No te lo pierdas!" image={fotoPelota} />
       <ServiciosCards title="RESERVA AHORA!" description="Ingresa el tipo de cancha, la fecha, la hora y descubri todas las opciones que tenemos para vos." image={fotoTienda} />
--- src/components/serviciosCards/ServiciosCards.jsx
+++ src/components/serviciosCards/ServiciosCards.jsx
@@ -1,16 +1,27 @@
-
-
 import { Card } from "flowbite-react";
 
-export const ServiciosCards = ({title, description, image}) => { return (
+export const ServiciosCards = ({ title, description, image }) => {
+  return (
+    <>
+      <Card
+        className="max-w-full h-[300px] w-[450px] mx-auto relative"
+        imgAlt="foto"
+      >
+        <img
+          src={image}
+          alt="foto"
+          className="top-0 left-0 z-0 object-cover w-full h-full mx-auto rounded"
+        />
 
-  <>
-  <Card className="max-w-sm h-[300px]  mx-auto relative hover:border-green-950 overflow-hidden" imgAlt="foto"> 
-  <img src={image} alt="foto" className="absolute top-0 left-0 z-0 object-cover w-full h-full mx-auto rounded"/>
-  <h5 className="text-2xl font-bold tracking-tight text-white dark:text-white z-10">{title}</h5>
-  <p className="font-normal text-white dark:text-gray-400 z-10 hover:text-black hover:font-bold hover:text-lg" >{description}</p>
-  </Card>
-  
-  </>
-
-)}
+        <div className="absolute text-center h-full w-full bg-transparent flex flex-col justify-center items-center z-10 hover:backdrop-blur">
+          <h5 className="text-2xl font-bold tracking-tight text-white dark:text-white">
+            {title}
+          </h5>
+          <p className="font-normal text-white dark:text-gray-400 p-5 mr-10 items-center">
+            {description}
+          </p>
+        </div>
+      </Card>
+    </>
+  );
+};
