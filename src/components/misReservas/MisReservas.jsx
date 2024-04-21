064e279c57f9d737105c9b39be9cf820838a7a99
Enzo Castro <enzocastro110@gmail.com>
2024-04-20 23:23:54 -0300

    en este caso no figuran reservas, estoy tomando un usuario con ID 7. Este componente lo puse en la view del home para poder visualizarlo

--- src/components/misReservas/MisReservas.jsx
+++ src/components/misReservas/MisReservas.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import { Button, Card } from "flowbite-react";
+import React, { useState, useEffect } from 'react';
+import { Card, Button } from "flowbite-react";
+import axios from 'axios';
 
 export const MisReservas = () => {
+  const [reservas, setReservas] = useState([]);
+
+  useEffect(() => {
+    const usuarioId = 7;
+    axios.get(`https://futbol-arena-back.onrender.com/users/${usuarioId}`)
+      .then(response => {
+        setReservas(response.data.reservas);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  if (reservas.length === 0) {
+    return (
+      <div className="text-center mt-4">
+        <p className="text-gray-700">Aún no tienes reservas realizadas.</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="px-4 mt-4 rounded-lg" >
-      <Card className="max-w-sm border border-black">
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900">
-          Tu reserva:
-        </h5>
-        <p className="font-normal text-gray-700">
-          Aquí voy a agregar una lista con la información de la reserva
-        </p>
-        <Button>
-          Read more
-          <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </Button>
-      </Card>
+    <div className="px-4 mt-4 rounded-lg">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {reservas.map((reserva, index) => (
+          <div key={index} className="mb-4">
+            <Card className="max-w-sm border border-black">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900">
+                Reserva #{reserva.cancha}
+              </h5>
+              <p className="font-normal text-gray-700">Fecha: {reserva.fecha}</p>
+              <p className="font-normal text-gray-700">Horario: {reserva.horario}</p>
+              <Button>Editar Reserva</Button>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
